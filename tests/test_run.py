4dc7272748baf82bfd197028ce86d149b5536fe4
Arnault Chazareix <arnaultc@sicara.com>
2023-01-14 02:53:47 +0100

    feat(): add support and tests for add_script_run_ctx
    fix(): remove error adding bad code to tests
    
    feat(): remove suport for streamlit<0.65 and add test for streamlit==1.4.*
    
    fix(): typo in test and mock threads
    
    fix(): bad rebase
    
    fix(): add missing init

--- tests/test_run.py
+++ tests/test_run.py
@@ -32,3 +32,22 @@ def test_get_script_run_ctx_in_thread():
     from st_compat.runtime.scriptrunner import get_script_run_ctx
 
     assert get_script_run_ctx() is sentinel.script_run_context
+
+
+@mock_for_streamlit_context
+def test_add_script_run_ctx_in_thread():
+    from st_compat.runtime.scriptrunner import (
+        SCRIPT_RUN_CONTEXT_ATTR_NAME,
+        add_script_run_ctx,
+    )
+
+    add_script_run_ctx(ctx=sentinel.script_run_context)
+    assert getattr(threading.current_thread(), SCRIPT_RUN_CONTEXT_ATTR_NAME) is sentinel.script_run_context
+
+
+@mock_for_streamlit_context
+def test_run_ctx_integration():
+    from st_compat.runtime.scriptrunner import add_script_run_ctx, get_script_run_ctx
+
+    add_script_run_ctx(ctx=sentinel.script_run_context)
+    assert get_script_run_ctx() is sentinel.script_run_context
