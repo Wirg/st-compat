d09437b0258be18c999cd498fa59f1c4af5a69c4
Arnault <Wirg@users.noreply.github.com>
2023-01-16 18:40:33 +0100

    feat(): add support for is_running_with_streamlit (#9)
    * feat(): add support for is_running_with_streamlit
    
    * fix(): fix is_running_with_streamlit between st versions

--- st_compat/runtime/scriptrunner.py
+++ st_compat/runtime/scriptrunner.py
@@ -1,8 +1,9 @@
+# pylint: disable=no-name-in-module,import-error,import-outside-toplevel
+
 from packaging import version
 
 from st_compat.version import st_version
 
-# pylint: disable=no-name-in-module,import-error,import-outside-toplevel
 if st_version >= version.parse("1.12.0"):
     from streamlit.runtime.scriptrunner.script_run_context import (
         SCRIPT_RUN_CONTEXT_ATTR_NAME,
@@ -29,10 +30,22 @@ elif st_version >= version.parse("0.65"):
     from streamlit.report_thread import get_report_ctx as get_script_run_ctx
 else:
     raise ModuleNotFoundError(f"Not supporting streamlit version before 0.65, found: {st_version}")
-# pylint: enable=no-name-in-module,import-error,import-outside-toplevel
+
+
+def is_running_with_streamlit() -> bool:
+    if st_version >= version.parse("1.14.0"):
+        from streamlit.runtime import exists
+
+        return exists()
+
+    from streamlit import _is_running_with_streamlit
+
+    return _is_running_with_streamlit
+
 
 __all__ = [
     "add_script_run_ctx",
     "get_script_run_ctx",
+    "is_running_with_streamlit",
     "SCRIPT_RUN_CONTEXT_ATTR_NAME",
 ]
